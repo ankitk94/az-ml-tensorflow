54196b7d66918e38ede0f6658b962a6fc91e6264
Ankit Khokhar <ankhokha@microsoft.com>
2018-11-06 14:42:14 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,142 +1,85 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license.
-
+import tensorflow as tf
 from sklearn.datasets import load_boston
-from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import scale
+from matplotlib import pyplot as plt
 from azureml.core.run import Run
-from sklearn.externals import joblib
-import os
-import numpy as np
-import pickle
 
+run = Run.get_submitted_run()
 os.makedirs('./outputs', exist_ok=True)
 
-run = Run.get_submitted_run()
+def calc(x, y):
+    # Returns predictions and error
 
-X, y = load_boston(return_X_y=True)
+    predictions = tf.add(b, tf.matmul(x, w))
+    error = tf.reduce_mean(tf.square(y - predictions))
 
-run = Run.get_submitted_run()
+    return [ predictions, error ]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size=0.2,
-                                                    random_state=0)
-data = {"train": {"X": X_train, "y": y_train},
-        "test": {"X": X_test, "y": y_test}}
+# Get the data
+total_features, total_prices = load_boston(True)
 
-reg = Ridge(alpha=0.65)
-reg.fit(data["train"]["X"], data["train"]["y"])
+# Keep 300 samples for training
+train_features = scale(total_features[:300])
+train_prices = total_prices[:300]
 
+# Keep 100 samples for validation
+valid_features = scale(total_features[300:400])
+valid_prices = total_prices[300:400]
 
-import numpy as np
-import argparse
-import os
-import tensorflow as tf
+# Keep remaining samples as test set
+test_features = scale(total_features[400:])
+test_prices = total_prices[400:]
 
-from azureml.core import Run
-from utils import load_data
-
-print("TensorFlow version:", tf.VERSION)
+w = tf.Variable(tf.truncated_normal([13, 1], mean=0.0, stddev=1.0, dtype=tf.float64), name="W")
+b = tf.Variable(tf.zeros(1, dtype = tf.float64), name="B")
 
 
-print(X_train.shape, y_train.shape, X_test.shape, y_test.shape, sep='\n')
-training_set_size = X_train.shape[0]
 
-n_inputs = 1 * 13
-n_h1 = 20
-n_h2 = 30
-n_outputs = 100
-learning_rate = 0.65
-n_epochs = 100
-batch_size = 20
 
-with tf.name_scope('network'):
-    # construct the DNN
-    X = tf.placeholder(tf.float32, shape=(None, n_inputs), name='X')
-    y = tf.placeholder(tf.int64, shape=(None), name='y')
-    h1 = tf.layers.dense(X, n_h1, activation=tf.nn.relu, name='h1')
-    h2 = tf.layers.dense(h1, n_h2, activation=tf.nn.relu, name='h2')
-    output = tf.layers.dense(h2, n_outputs, name='output')
+y, cost = calc(train_features, train_prices)
 
-with tf.name_scope('train'):
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=output)
-    loss = tf.reduce_mean(cross_entropy, name='loss')
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    train_op = optimizer.minimize(loss)
+# Feel free to tweak these 2 values:
 
-with tf.name_scope('eval'):
-    correct = tf.nn.in_top_k(output, y, 1)
-    acc_op = tf.reduce_mean(tf.cast(correct, tf.float32))
+learning_rate = 0.0025
+
+epochs = 3000
+
+points = [[], []] # You'll see later why I need this
+
 
 
 init = tf.global_variables_initializer()
 saver = tf.train.Saver()
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-# start an Azure ML run
 run = Run.get_context()
-print("Starting session")
+
+
+
 with tf.Session() as sess:
-    init.run()
-    for epoch in range(n_epochs):
 
-        # randomly shuffle training set
-        indices = np.random.permutation(training_set_size)
-        X_train = X_train[indices]
-        y_train = y_train[indices]
+    sess.run(init)
 
-        # batch index
-        b_start = 0
-        b_end = b_start + batch_size
-        print("inside epoch")
-        for _ in range(training_set_size // batch_size):
-            # get a batch
-            X_batch, y_batch = X_train[b_start: b_end], y_train[b_start: b_end]
+    for i in list(range(epochs)):
 
-            # update batch index for the next batch
-            b_start = b_start + batch_size
-            b_end = min(b_start + batch_size, training_set_size)
+        sess.run(optimizer)
 
-            # train
-            print("Will run session")
-            print (X_batch)
-            print(y_batch)
-            sess.run(train_op, feed_dict={X: X_batch, y: y_batch})
-            print("Done with run")
-        # evaluate training set
-        acc_train = acc_op.eval(feed_dict={X: X_batch, y: y_batch})
-        # evaluate validation set
-        acc_val = acc_op.eval(feed_dict={X: X_test, y: y_test})
+        if i % 10 == 0.:
+            points[0].append(i+1)
+            points[1].append(sess.run(cost))
 
-        # log accuracies
-        run.log('training_acc', np.float(acc_train))
-        run.log('validation_acc', np.float(acc_val))
-        print(epoch, '-- Training accuracy:', acc_train, '\b Validation accuracy:', acc_val)
-        y_hat = np.argmax(output.eval(feed_dict={X: X_test}), axis=1)
+        if i % 100 == 0:
+            print(sess.run(cost))
 
-    run.log('final_acc', np.float(acc_val))
+    plt.plot(points[0], points[1], 'r--')
+    plt.axis([0, epochs, 50, 600])
+    plt.show()
 
-    os.makedirs('./outputs/model', exist_ok=True)
-    # files saved in the "./outputs" folder are automatically uploaded into run history
-    saver.save(sess, './outputs/model/mnist-tf.model')
+    valid_cost = calc(valid_features, valid_prices)[1]
+
+    print('Validation error =', sess.run(valid_cost), '\n')
+
+    test_cost = calc(test_features, test_prices)[1]
+
+    print('Test error =', sess.run(test_cost), '\n')
     saver.save(sess, './outputs/mnist-tf.model')
-'''
-preds = reg.predict(data["test"]["X"])
-mse = mean_squared_error(preds, data["test"]["y"])
-
-run.log("mse", mse)
-print(mse)
-
-
-model_path = 'model.pkl'
-f = open(model_path, 'wb')
-pickle.dump(reg, f)
-
-
-
-with open(model_path, "wb") as file:
-    from sklearn.externals import joblib
-    joblib.dump(reg, file)
-run.upload_file(model_path,  model_path)
-os.remove(model_path)
-'''
